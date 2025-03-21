a7595d48b81ed04d36f5b31180a8e03fd496ac9c
D3k3s <sund3shine@outlook.com>
2025-03-21 19:19:04 +0300

    change system messages prefix

--- src/main/java/meteordevelopment/meteorclient/utils/player/ChatUtils.java
+++ src/main/java/meteordevelopment/meteorclient/utils/player/ChatUtils.java
@@ -38,7 +38,7 @@ public class ChatUtils {
         PREFIX = Text.empty()
             .setStyle(Style.EMPTY.withFormatting(Formatting.GRAY))
             .append("[")
-            .append(Text.literal("Meteor").setStyle(Style.EMPTY.withColor(TextColor.fromRgb(D3.ADDON.color.getPacked()))))
+            .append(Text.literal(D3.NAME).setStyle(Style.EMPTY.withColor(TextColor.fromRgb(D3.ADDON.color.getPacked()))))
             .append("] ");
     }
 
