9e7834fcad743f44ae5febc1ead60f840c3b1214
D3k3s <sund3shine@outlook.com>
2025-04-28 01:22:47 +0300

    aimassist, target highlight

--- src/main/java/meteordevelopment/meteorclient/systems/modules/combat/AimAssist.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/combat/AimAssist.java
@@ -7,12 +7,18 @@ package meteordevelopment.meteorclient.systems.modules.combat;
 
 import java.util.*;
 
+import meteordevelopment.meteorclient.events.render.Render3DEvent;
+import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.renderer.Renderer3D;
 import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.utils.entity.*;
 import meteordevelopment.meteorclient.utils.player.*;
+import meteordevelopment.meteorclient.utils.render.color.*;
+import meteordevelopment.orbit.EventHandler;
 import net.minecraft.entity.*;
+import net.minecraft.util.math.MathHelper;
 
 public class AimAssist extends Module {
 
@@ -24,12 +30,14 @@ public class AimAssist extends Module {
 
 	private SettingGroup rotationSettings = settings.createGroup("Rotation");
 
+	private SettingGroup renderSettings = settings.createGroup("Render");
+
 	private final Setting<Set<EntityType<?>>> entities = targetSettings.add(new EntityTypeListSetting.Builder()
 			.name("Entities").description("Target entities").onlyAttackable().defaultValue(EntityType.PLAYER).build());
 
-	private final Setting<SortPriority> priority = targetSettings.add(
-			new EnumSetting.Builder<SortPriority>().name("Priority").description("How to filter targets within range")
-					.defaultValue(SortPriority.ClosestAngle).build());
+	private final Setting<SortPriority> priority = targetSettings
+			.add(new EnumSetting.Builder<SortPriority>().name("Priority")
+					.description("How to filter targets within range").defaultValue(SortPriority.ClosestAngle).build());
 
 	private final Setting<Boolean> ignoreInvisible = targetSettings.add(new BoolSetting.Builder()
 			.name("Ignore invisible").description("Ignore invisible entities").defaultValue(true).build());
@@ -40,8 +48,67 @@ public class AimAssist extends Module {
 	private final Setting<Integer> lookupAngle = targetSettings.add(new IntSetting.Builder().name("Lookup angle")
 			.description("Target lookup FOV").range(0, 180).defaultValue(50).sliderRange(0, 180).build());
 
+	private final Setting<Boolean> highlightTarget = renderSettings
+			.add(new BoolSetting.Builder().name("Highlight target").defaultValue(true).build());
+
+	private final Setting<Integer> cylinderSegments = renderSettings
+			.add(new IntSetting.Builder().name("Target cylinder segments").range(0, 80).defaultValue(50)
+					.sliderRange(4, 100).visible(highlightTarget::get).build());
+
+	private final Setting<SettingColor> cylinderColor = renderSettings
+			.add(new ColorSetting.Builder().name("Cylinder color").defaultValue(Color.CYAN).build());
+
 	public AimAssist() {
 		super(Categories.Combat, NAME, DESCRIPTION);
+	}
+
+	private Optional<Entity> target = Optional.empty();
+
+	@EventHandler
+	private void tick(TickEvent.Pre event) {
+		target = findTarget();
+	}
+
+	@EventHandler
+	private void render(Render3DEvent event) {
+		if (target.isPresent() && highlightTarget.get()) {
+			renderTarget(event.renderer, target.get(), event.tickDelta);
+		}
+	}
+
+	private void renderTarget(Renderer3D renderer, Entity entity, float tickDelta) {
+		final double TWICE_PI = Math.PI * 2;
+
+		Color color = cylinderColor.get();
+		double entityX = MathHelper.lerp(tickDelta, entity.lastRenderX, entity.getX());
+		double entityY = MathHelper.lerp(tickDelta, entity.lastRenderY, entity.getY());
+		double entityZ = MathHelper.lerp(tickDelta, entity.lastRenderZ, entity.getZ());
+		double radius = entity.getWidth() * 1.1;
+		double height = entity.getHeight();
+		double offset = height * 0.1;
+		double segments = cylinderSegments.get();
+
+		double lastX = entityX + radius;
+		double lastZ = entityZ;
+
+		for (int i = 0; i < segments; i++) {
+			double x = entityX + radius * Math.cos(i * TWICE_PI / segments);
+			double z = entityZ + radius * Math.sin(i * TWICE_PI / segments);
+
+			// bottom circle
+			double bottomY = entityY + offset;
+			renderer.line(lastX, bottomY, lastZ, x, bottomY, z, color);
+
+			// top circle
+			double topY = entityY + height - offset;
+			renderer.line(lastX, topY, lastZ, x, topY, z, color);
+
+			// vertical line
+			renderer.line(x, bottomY, z, x, topY, z, color);
+
+			lastX = x;
+			lastZ = z;
+		}
 	}
 
 	private Optional<Entity> findTarget() {
@@ -60,7 +127,7 @@ public class AimAssist extends Module {
 		if (!entities.get().contains(entity.getType())) {
 			return false;
 		}
-		
+
 		if (!PlayerUtils.canSeeEntity(entity)) {
 			return false;
 		}
@@ -72,8 +139,8 @@ public class AimAssist extends Module {
 		if (entity.distanceTo(mc.player) > lookupRadius.get()) {
 			return false;
 		}
-		
-		if(Rotations.getYawDistanceTo(entity.getPos()) > lookupAngle.get()) {
+
+		if (Rotations.getYawDistanceTo(entity.getPos()) > lookupAngle.get()) {
 			return false;
 		}
 
